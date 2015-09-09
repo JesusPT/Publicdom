ac4188c416a30e8b3c208fdf1fed6886486968d9
ShinigamiJE <pak.jesus@hotmail.com>
2015-09-08 19:33:07 -0500

    Proyecto PublicDom

---
+++ js/index.js
@@ -0,0 +1,15 @@
+$(document).on("ready",function(){
+		$("#btoReg").click(function(){
+			$("#contenedor").css("display","inline-block");
+		});
+		$("#btoIni").click(function(){
+			$("#ventaIni").css("display","inline-block");
+		});
+		$(".close").click(function(){
+			$("#contenedor").css("display","none");
+		});
+		$("#closeIni").click(function(){
+			$("#ventaIni").css("display","none");
+		});
+});
+

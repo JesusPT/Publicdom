7ac52898ff357bd75d5206aab361eed67103bcbf
ShinigamiJE <pak.jesus@hotmail.com>
2015-09-08 23:39:29 -0500

    php

---
+++ php/registro.php
@@ -0,0 +1,19 @@
+<?php
+
+$name = $_POST['name'];
+$AP = $_POST['AP'];
+$AM = $_POST['AM'];
+$User = $_POST['user'];
+$Pass = $_POST['pass'];
+$rPass = $_POST['rPass'];
+$Age = $_POST['Age'];
+$Email = $_POST['Email'];
+
+if ($enlace = new sqli(localhost,root)) {
+  $consulta = "insert into usuarios() values()";
+  if ($enlace -> query("call registro('$User','$name','$AP','$AM','$Age','$Email','$Pass')")) {
+    echo 1;
+  }else{echo 0;}
+}else{echo 0;}
+
+?>
--- php/sesion.php
+++ php/sesion.php
@@ -4,12 +4,21 @@
 	$pass = $_POST['pass'];
 
 if ($enlace = new mysqli("localhots","root")) {//Conexion con la DB
-	$consulta = "SELECT * FROM usuario where idUser = '$usuario', ";//Consulta SQL a ejecutar
+	$consulta = "SELECT * FROM usuario where idUser = '$usuario', pass = '$pass'";//Consulta SQL a ejecutar
 	if ($respuesta = $enlace->query($consulta)) {//Realizar consulta
 		if ($respuesta -> num_rows > 0) {//si la respuesta contiene alguna fila
-			if ($respuesta -> fetch_assoc()) {//obtener filas de resultado como arreglos asociativos
-
+			if ($filas = $respuesta -> fetch_assoc()) {//obtener filas de resultado como arreglos asociativos
+				session_start();
+				unset($_SESSION['perfil']);//limpiar el espacio 'perfil'
+				$_SESSION['perfil']['user'] = $_POST['user'];//Guardar nombre de usuario en el arreglo de sesiones
+				$_SESSION['perfil']['pass'] = $_POST['pass'];//Guardar contraseña en el arreglo de sesiones
+			}else{
+				//Error al obtener el arreglo asociativo
+				echo 0;
 			}
+		}else{
+			//Error no hay filas
+			echo 0;
 		}
 	}else{
 		//Error al realizar la consulta
@@ -19,6 +28,5 @@ if ($enlace = new mysqli("localhots","root")) {//Conexion con la DB
 	//Error en la conexión
 	echo 0;
 }
-
 
 ?>

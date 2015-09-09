93bffad7dee53c01f99c6173c223dbaf74997178
ShinigamiJE <pak.jesus@hotmail.com>
2015-09-08 21:10:30 -0500

    php

--- php/sesion.php
+++ php/sesion.php
@@ -1,10 +1,16 @@
 <?php
 
+	$usuario = $_POST['user'];
+	$pass = $_POST['pass'];
 
-if ($enlace = new mysqli("localhots","root")) {
-	$consulta = "SELECT * FROM usuario";
-	if ($respuesta = $enlace->query($consulta)) {
-		
+if ($enlace = new mysqli("localhots","root")) {//Conexion con la DB
+	$consulta = "SELECT * FROM usuario where idUser = '$usuario', ";//Consulta SQL a ejecutar
+	if ($respuesta = $enlace->query($consulta)) {//Realizar consulta
+		if ($respuesta -> num_rows > 0) {//si la respuesta contiene alguna fila
+			if ($respuesta -> fetch_assoc()) {//obtener filas de resultado como arreglos asociativos
+
+			}
+		}
 	}else{
 		//Error al realizar la consulta
 		echo 0;

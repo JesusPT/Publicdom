f9aae5acfa8153bd0803f71c8ef523e0819f9ece
Jocs84 <joluloca666@gmail.com>
2015-09-22 21:21:37 -0500

    Validacion

--- php/registro.php
+++ php/registro.php
@@ -3,31 +3,24 @@
 $name = $_POST['name'];
 $paterno = $_POST['AP'];
 $materno = $_POST['AM'];//nombre del usuario
-$User = $_POST['User'];//usuario
-$Pass = /*md5(*/$_POST['Pass']/*)*/;//contraseña
+//$User = md5($_POST['User']);//usuario
+$User = $_POST['User'];
+//$Pass = md5($_POST['Pass']);//contraseña
+$Pass = $_POST['Pass'];
 $Age = $_POST['Age'];//edad del usuario
 $Email = $_POST['Email'];//correo electronico
 
-/*}
-  function test_input($data){
-  $data = trim($data);
-  $data = stripslashes($data);
-  $data = htmlspecialchars($data);
-  return $data;
-}
-
-*/
  // Validacion de Formulario
- if (preg_match("/^(([A-Z]{1})([a-z|ñáéíóú]{1,13}[\s]?)){1,5}$/",$name)) {//2 Validacion(Comienza con mayuscula)
+ if (preg_match("/^(([A-Z]{1})([a-z|ñáéíóú]{1,13}[\s]?)){1,5}$/",$name)) {//2
    if (preg_match("/^[A-Z]{1}[a-z|ñáéíóú]*$/",$paterno)) {//3
      if (preg_match("/^[A-Z]{1}[a-z|ñáéíóú]*$/",$materno)) {//4
        if (preg_match("/^[a-zA-Z0-9|_|#]{3,11}$/", $User)) {//5
          if( preg_match ("/^[\w|-]{5,18}$/",$Pass)){//6
            if(preg_match("/^[\d]{2}$/",$Age)){//7
-             if (filter_var(Email, FILTER_VALIDATE_EMAIL)){//8
+             if (filter_var($Email, FILTER_VALIDATE_EMAIL)){//8
                   //echo "dentro de email";
                if ($enlace = new mysqli("localhost","root","","publidom")) {
-                 if ($enlace -> query("call registro('$User','$name. .$paterno. .$materno','$Age','$Email','$Pass')")) {
+                 if ($enlace -> query("call registro('$User','$name $paterno $materno','$Age','$Email','$Pass')")) {
                    echo 1;
                  }else{
                    echo 0;
@@ -56,15 +49,5 @@ $Email = $_POST['Email'];//correo electronico
    } else{
      echo 2;
    }
-/*
-if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
-}
 
-
-if ($enlace = new mysqli("localhost","root","","publidom")) {
-  if ($enlace -> query("call registro('$User','$name','$Age','$Email','$Pass')")) {
-    echo 1;
-  }else{echo 0;}
-}else{echo 0;}
-*/
 ?>

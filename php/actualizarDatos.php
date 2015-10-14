3c849b69136930a7d022f02b8f3c7269825bdab1
Jocs84 <joluloca666@gmail.com>
2015-10-14 07:21:14 -0500

    Actualizar Datos

---
+++ php/actualizarDatos.php
@@ -0,0 +1,32 @@
+<?php
+
+$user = $_POST['User']
+$pass = $_POST['Pass'];
+$nPass = $_POST['newPass'];
+$email = $_POST['Email'];
+session_start();
+//$_SESSION ['perfil']['pass']
+if($pass == ($_SESSION ['perfil']['pass'])){
+  ///Cambio de Nombre de usuario
+  if ($user == null) {//2
+
+  }elseif((preg_match("/^[a-zA-Z0-9|_|#]{3,11}$/", $user))){
+      echo 2;
+    }
+    /////Cambio de password_hash
+  if($nPass == null){//3
+
+  }elseif(preg_match ("/^[\w|-]{5,18}$/",$pass){
+     echo 3;
+   }
+   /////Cambio de Email
+  if(email == null){//4
+
+  }elseif (filter_var($Email, FILTER_VALIDATE_EMAIL)) {
+     echo 4;
+}
+}
+
+
+
+?>

b860053d47888e23337e5fb36ac2de29fc6301d5
Jocs84 <joluloca666@gmail.com>
2015-11-12 18:30:37 -0600

    his

--- php/historial.php
+++ php/historial.php
@@ -4,7 +4,11 @@
   $user = $_SESSION['perfil']['user'];
 
   if (@$enlace = new mysqli("localhost","root","","publidom")) {
-    $consulta = "SELECT nomProducto,descripcion from producto pro INNER JOIN historiales his ON pro.idProducto = his.idProducto and idUsuario = '".$user."'";
+    $consulta = "SELECT nomProducto,descripcion from producto pro
+      INNER JOIN historiales his ON
+      pro.idProducto = his.idProducto and idUsuario = '".$user."' or
+      nomServicio,descripcion from servicio serv INNER JOIN historiales his ON
+      serv.idServicio = his.idServicio and idUsuario = '".$user."'";
     if (@$respuesta = $enlace -> query($consulta)) {
       if ($respuesta -> num_rows > 0) {
         while ($filas = $respuesta -> fetch_assoc()) {

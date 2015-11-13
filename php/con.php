2dd3b86e53037302ce15c82544fd658a55dc85d7
ShinigamiJE <pak.jesus@hotmail.com>
2015-11-12 18:17:49 -0600

    TutoLuisLuisLuisLuisLuis

---
+++ php/con.php
@@ -0,0 +1,28 @@
+<?php
+
+
+$con = new mysqli("localhost","root","","publidom");
+
+$consql = "SELECT idUsuario from usuario";
+
+$makeconsul = $con -> query($consql);
+
+$varcon = $makeconsul -> fetch_assoc();
+
+
+    while ($varcon = $makeconsul -> fetch_assoc()) {
+
+      //foreach ($varcon as $key) {
+        //echo $key;
+        //echo "\n";
+      //}
+      echo $varcon['idUsuario'] . " ";
+    }
+
+    //echo $varcon['nomUsuario'];
+
+
+
+
+
+ ?>
